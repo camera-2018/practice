ca7c23b8726eacd2383b0f6488bea60e72855fe4
camera-2018 <2907618001@qq.com>
2021-10-22 15:38:26 +0800

    1.2

--- XiangYang_Todo_List/README.md
+++ XiangYang_Todo_List/README.md
@@ -2,13 +2,29 @@
 	npm install
 # 使用
 	node todolist.js help
-##### 菜
-2021年10月20日13:35:00 ~ 2021年10月21日10:27:40
 
+	node todolist.js <method> [options...]                     
+	<status> = todo / doing / done                              
+	node todolist.js add <status> <id> <content>   添加任务
+	node todolist.js ls                            列出所有任务
+	node todolist.js ls --status=<status>          列出指定状态任务
+	node todolist.js <status> <id>                 将该id对应的任务置为对应状态
+	node todolist.js delete <id>                   删除该id的任务
+	node todolist.js help                          帮助面板
 
 # 目录
-│  package-lock.json
-│  package.json
-│  README.md
-│  todo.yaml
-│  todolist.js
+	│  package.json
+	│  README.md
+	│  todo.yaml
+	│  todolist.js
+
+
+# 更新记录
+- 1.1版本
+- 修了一堆bug
+
+- 1.2版本 
+- 增加了炫彩代码 **史诗级更新 <font color=red>R</font><font color=green>G</font><font color=blue>B</font>增加50%性能**
+- 炫彩的使用方法 https://www.cnblogs.com/xiaoqiangink/p/12718524.html
+- 使用了字符串模板 template string
+- 增加使用了 === 严格判断 （不会用 会爆炸）
--- XiangYang_Todo_List/todolist.js
+++ XiangYang_Todo_List/todolist.js
@@ -1,13 +1,19 @@
 /*
  * @Author: xiangyang
  * @Date: 2021-10-20 15:22:50
- * @LastEditTime: 2021-10-21 17:46:55
+ * @LastEditTime: 2021-10-22 15:09:52
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \XiangYang_Todo_List\todolist.js
  */
+const green = "\033[32m";     
+const yellow = "\033[33m";
+const azure = "\033[36m";
+const red = "\033[31m";
+const fontfoot = "\033[0m";
 const fs = require('fs');
 const yaml = require('js-yaml');
+
 function write(data){                   //写入
     try {
         fs.writeFileSync('todo.yaml',yaml.dump(data),'utf8');
@@ -31,95 +37,12 @@ function read(){                           //读取
 
 let todojson = read();
 
-//命令行控制
-const args = process.argv.slice(2)
-const argsplus = require('minimist')(process.argv.slice(2));
-if(args[0]=='ls'){
-    
-    if(argsplus['status']=='todo'){
-        console.log("---------------todo---------------");
-        todojson.todo.forEach(element => {
-            console.log(element.id+'                            '+element.content);
-        });
-    }
-    if(argsplus['status']=='doing'){
-        console.log("---------------doing---------------");
-        todojson.doing.forEach(element => {
-            console.log(element.id+'                            '+element.content);
-        });
-    }
-    if(argsplus['status']=='done'){
-        console.log("---------------done---------------");
-        todojson.done.forEach(element => {
-            console.log(element.id+'                            '+element.content);
-        });
-    }
-    if(argsplus['status']==null){
-        print();
-    }
-}
-else if(args[0]=='help'){
-    
-    help();
-}
-//任务标签转移
-else if(args[0]=='done'){
-    let b = args[1];
-    b.toString();
-    done(b);
-    write(todojson);
-    print();
-}
-else if(args[0]=='todo'){
-    let b = args[1];
-    b.toString();
-    todo(b);
-    write(todojson);
-    print();
-}
-else if(args[0]=='doing'){
-    let b = args[1];
-    b.toString();
-    doing(b);
-    write(todojson);
-    print();
-}
-else if(args[0]=='delete'){
-    let b =args[1];
-    b.toString();
-    todojson.todo.forEach((element,index,arr) => {
-        if(element.id==b){
-            arr.splice(index,1);
-        }
-    });
-    todojson.doing.forEach((element,index,arr) => {
-        if(element.id==b){
-            arr.splice(index,1);
-        }
-    });
-    todojson.done.forEach((element,index,arr) => {
-        if(element.id==b){
-            arr.splice(index,1);
-        }
-    });
-    write(todojson);
-    print();
-}
-else if(args[0]=='add'){
-    let addstatus = args[1];
-    let addid = args[2];
-    let content = args[3];
-    
-    add(addstatus,addid,content);
-    write(todojson);
-    print();
-}
-else{
-    console.log("输入有误");
-    help();
-}
-
 function done(b){
+    todojson.done.forEach((element,index,arr) => {     //done到done
+        if(element.id==b){
+            console.log(`${azure+"Already Done!"+fontfoot}`)
+        }
+    });
     todojson.todo.forEach((element,index,arr) => {     //todo到done
         if(element.id==b){
             todojson.done.push(element);
@@ -132,14 +55,14 @@ function done(b){
             arr.splice(index,1);
         }
     });
-    todojson.done.forEach((element,index,arr) => {     //done到done
-        if(element.id==b){
-            console.log("already done")
-        }
-    });
 }
 
 function todo(b){
+    todojson.todo.forEach((element,index,arr) => {     //todo到todo
+        if(element.id==b){
+            console.log(`${azure+"Already Todo!"+fontfoot}`)
+        }
+    });
     todojson.done.forEach((element,index,arr) => {     //done到todo
         if(element.id==b){
             todojson.todo.push(element);
@@ -152,14 +75,14 @@ function todo(b){
             arr.splice(index,1);
         }
     });
-    todojson.todo.forEach((element,index,arr) => {     //todo到todo
-        if(element.id==b){
-            console.log("already todo")
-        }
-    });
 }
 
 function doing(b){
+    todojson.doing.forEach((element,index,arr) => {     //doing到doing
+        if(element.id==b){
+            console.log(`${azure+"Already Doing!"+fontfoot}`)
+        }
+    });
     todojson.todo.forEach((element,index,arr) => {     //todo到doing
         if(element.id==b){
             todojson.doing.push(element);
@@ -172,24 +95,19 @@ function doing(b){
             arr.splice(index,1);
         }
     });
-    todojson.doing.forEach((element,index,arr) => {     //doing到doing
-        if(element.id==b){
-            console.log("already doing")
-        }
-    });
 }
 
 //添加 语法为 node xxx.js add <status> <id> <content>
 function add(addstatus,addid,content1){
-    if(addstatus=='todo'){
+    if(addstatus==='todo'){
         let newobject = {id:parseInt(addid),content:content1};
         todojson.todo.push(newobject);
     }
-    if(addstatus=='doing'){
+    if(addstatus==='doing'){
         let newobject = {id:parseInt(addid),content:content1};
         todojson.doing.push(newobject);
     }
-    if(addstatus=='done'){
+    if(addstatus==='done'){
         let newobject = {id:parseInt(addid),content:content1};
         todojson.done.push(newobject);
     }
@@ -207,21 +125,103 @@ function help(){
         "node todolist.js help                          帮助面板",
     ]
     help.forEach(element => {
-        console.log(element);
+        console.log(`${yellow+element+fontfoot}`);
     });
 }
 
-function print(){
-    console.log("---------------todo---------------");
+function printTodo(){
+    console.log(`---------------${green+"todo"+fontfoot}---------------`);
     todojson.todo.forEach(element => {
-        console.log(element.id+'                            '+element.content);
-    });
-    console.log("---------------doing---------------");
-    todojson.doing.forEach(element => {
-        console.log(element.id+'                            '+element.content);
-    });
-    console.log("---------------done---------------");
-    todojson.done.forEach(element => {
-        console.log(element.id+'                            '+element.content);
+        console.log(`${yellow+element.id+fontfoot}${' '.padEnd(28,' ')}${azure+element.content+fontfoot}`);
     });
 }
+function printDoing(){
+    console.log(`---------------${green+"doing"+fontfoot}---------------`);
+    todojson.doing.forEach(element => {
+        console.log(`${yellow+element.id+fontfoot}${' '.padEnd(28,' ')}${azure+element.content+fontfoot}`);
+    });
+}
+function printDone(){
+    console.log(`---------------${green+"done"+fontfoot}---------------`);
+    todojson.done.forEach(element => {
+        console.log(`${yellow+element.id+fontfoot}${' '.padEnd(28,' ')}${azure+element.content+fontfoot}`);
+    });
+}
+function printAll(){
+    printTodo();
+    printDoing();
+    printDone();
+}
+//命令行控制
+const args = process.argv.slice(2)
+const argsplus = require('minimist')(process.argv.slice(2));
+if(args[0]=='ls'){
+    if(argsplus['status']==null){
+        printAll();
+    }
+    else if(argsplus['status']==='todo'){
+        printTodo();
+    }
+    else if(argsplus['status']==='doing'){
+        printDoing();
+    }
+    else if(argsplus['status']==='done'){
+        printDone();
+    }
+    else{
+        console.log(`${red+'输入有误!'+fontfoot}`);
+    }
+}
+if(args[0]==='help'){
+    help();
+}
+//任务标签转移
+if(args[0]==='done'){
+    let b = args[1];
+    done(b);
+    write(todojson);
+    printAll();
+}
+if(args[0]==='todo'){
+    let b = args[1];
+    todo(b);
+    write(todojson);
+    printAll();
+}
+if(args[0]==='doing'){
+    let b = args[1];
+    doing(b);
+    write(todojson);
+    printAll();
+}
+if(args[0]==='delete'){
+    let b =args[1];
+    todojson.todo.forEach((element,index,arr) => {
+        if(element.id==b){
+            arr.splice(index,1);
+        }
+    });
+    todojson.doing.forEach((element,index,arr) => {
+        if(element.id==b){
+            arr.splice(index,1);
+        }
+    });
+    todojson.done.forEach((element,index,arr) => {
+        if(element.id==b){
+            arr.splice(index,1);
+        }
+    });
+    write(todojson);
+    printAll();
+}
+if(args[0]==='add'){
+    let addstatus = args[1];
+    let addid = args[2];
+    let content = args[3];
+    
+    add(addstatus,addid,content);
+    write(todojson);
+    printAll();
+}
+
+
